30718c3b24082a727b5ac3bab59c16901e698954
MuchMercyx <muchxmercy@gmail.com>
2025-11-22 16:03:06 -0500

    Initial commit - Godestone scraper for FFXIV

--- api/character.go
+++ api/character.go
@@ -4,21 +4,19 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"strconv"
-
 	"github.com/karashiiro/bingode"
 	"github.com/xivapi/godestone/v2"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	// Hardcoded character ID
+	// hardcoded ID for your character
 	charID := uint32(28293967)
 
 	s := godestone.NewScraper(bingode.New(), godestone.EN)
 	c, err := s.FetchCharacter(charID)
 	if err != nil {
 		http.Error(w, "Error fetching character", http.StatusInternalServerError)
-		log.Println("FetchCharacter error:", err)
+		log.Println(err)
 		return
 	}
 
