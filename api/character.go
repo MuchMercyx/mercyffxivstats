cbc87b53c61b5208fca4453b249644b6e49e9a34
MuchMercyx <muchxmercy@gmail.com>
2025-11-22 16:12:09 -0500

    Initial commit - Godestone scraper for FFXIV

--- api/character.go
+++ api/character.go
@@ -9,15 +9,17 @@ import (
 	"github.com/xivapi/godestone/v2"
 )
 
+// Handler is required by Vercel for serverless Go
 func Handler(w http.ResponseWriter, r *http.Request) {
-	// Hardcoded character ID for your account
+	s := godestone.NewScraper(bingode.New(), godestone.EN)
+
+	// Hard-coded character ID
 	charID := uint32(28293967)
 
-	s := godestone.NewScraper(bingode.New(), godestone.EN)
 	c, err := s.FetchCharacter(charID)
 	if err != nil {
 		http.Error(w, "Error fetching character", http.StatusInternalServerError)
-		log.Println(err)
+		log.Println("FetchCharacter error:", err)
 		return
 	}
 
